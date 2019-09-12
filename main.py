b4ab7c9e2d299fb386e550e512b6d389ead0b5e6
Tusk98 <henryracoomb@gmail.com>
2019-09-12 00:50:21 -0400

    Matplotlib to plot ohlc and volume into graph

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
 from matplotlib import style
+from mpl_finance import candlestick_ohlc
 
 style.use('ggplot')
 
-start = dt.datetime(2010,1,1)
-end = dt.datetime.today()
-
-# Dataframe
-#df = web.DataReader('VXUS', 'yahoo', start, end)
-
-#print(df.head(10))
-#print(df.tail(10))
-
-#df.to_csv("VXUS")
-
 df = pd.read_csv("VXUS.csv", parse_dates= True, index_col = 0)
 
-#print(df.head())
+# ohlc = open high low close
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
 
-df['Adj Close'].plot()
+df_ohlc.reset_index(inplace=True)
+
+# Setup dates for matplotlib since it doesn't use dt
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+# Setup axis
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan= 5, colspan= 1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan= 1, colspan= 1, sharex=ax1)
+# Display date nicely
+ax1.xaxis_date()
+
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
 plt.show()
-
-
