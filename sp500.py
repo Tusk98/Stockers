80a95aaa7ba0e974c923e292937641bcf6b0c93b
Tusk98 <henryracoomb@gmail.com>
2019-09-13 01:25:18 -0400

    Changed from sp500 to personally selected stocks + compile closing data function

--- sp500.py
+++ sp500.py
@@ -28,12 +28,18 @@ def save_sp500_symbols():
 
     return symbols
 
+def dump_own_stocks():
+    symb = ["BND", "VXUS", "ACB", "FB"]
+    with open("myStocks.pickle", "wb") as f:
+        pickle.dump(symb, f)
 
-def get_data_yahoo(reload_sp500 = False):
-    if reload_sp500:
-        symbols = save_sp500_symbols()
+
+def get_data_yahoo(reload_stocks = False):
+    # Check if myStocks file exist
+    if reload_stocks:
+        symbols = dump_own_stocks()
     else:
-        with open("sp500symbols.pickle", "wb") as f:
+        with open("myStocks.pickle", "rb") as f:
             symbols = pickle.load(f)
 
     if not os.path.exists('stocks_dfs'):
@@ -42,11 +48,40 @@ def get_data_yahoo(reload_sp500 = False):
     start = dt.datetime(2013,1,1)
     end = dt.datetime.today()
 
+    # For each symbol save their stock info in a the directory stocks_dfs
     for ticker in symbols:
         if not os.path.exists('stocks_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'yahoo', start, end)
-            df.to_csv('stocks_dfs/{}.csv'.format(ticker))
+            try:
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv('stocks_dfs/{}.csv'.format(ticker))
+            except:
+                print(ticker, " not found")
         else:
             print('Already have {}'.format(ticker))
 
-            
+
+def compile_data():
+    with open("myStocks.pickle", "rb") as f:
+        symbols = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(symbols):
+        df = pd.read_csv('stocks_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns = {'Adj Close' : ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('myStocks_joined_closes.csv')
+
+compile_data()
