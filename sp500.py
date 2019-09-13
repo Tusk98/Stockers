0f18a5298fda62617f15ab4be5f4bdbdf9bd0b41
Tusk98 <henryracoomb@gmail.com>
2019-09-13 00:34:53 -0400

    Created save sp500 csv function and function to check directory and get data

--- sp500.py
+++ sp500.py
@@ -1,8 +1,15 @@
 # Web scraper
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle
 import requests
 
+"""
+    Function specifically created to save the data for the SP500 symbols from Wikipedia
+"""
 def save_sp500_symbols():
     # Make a request from the wikipedia page
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -21,4 +28,25 @@ def save_sp500_symbols():
 
     return symbols
 
-save_sp500_symbols()
+
+def get_data_yahoo(reload_sp500 = False):
+    if reload_sp500:
+        symbols = save_sp500_symbols()
+    else:
+        with open("sp500symbols.pickle", "wb") as f:
+            symbols = pickle.load(f)
+
+    if not os.path.exists('stocks_dfs'):
+        os.makedirs('stocks_dfs')
+
+    start = dt.datetime(2013,1,1)
+    end = dt.datetime.today()
+
+    for ticker in symbols:
+        if not os.path.exists('stocks_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stocks_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+            
