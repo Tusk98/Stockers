902582b159df492a8577e532e1b32746449d1786
Tusk98 <henryracoomb@gmail.com>
2019-10-16 00:03:01 -0400

    renamed testsite to stockers_site

--- django/stockers_site/stockers_site/urls.py
+++ django/stockers_site/stockers_site/urls.py
@@ -1,4 +1,4 @@
-"""testsite URL Configuration
+"""stockers_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
